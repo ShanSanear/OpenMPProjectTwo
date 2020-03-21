4c83a3165488243b733496e9e1351ee7c90665c9
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-03-21 17:52:23 +0100

    Changed approach into class-based.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
-#include <list>
 #include <omp.h>
 #include <cmath>
 #include "../include/cxxopts.hpp"
 #include "argument_parsing.cpp"
 
-long double calculate_quadratic_formula(long double a, long double b, long double c, long double x) {
+class IntegralCalculator {
+private:
+    long double a, b, c;
+    long double starting_point, ending_point;
+    long double dx;
+    int n;
+
+    long double calculate_formula(long double x) const;
+
+public:
+    IntegralCalculator(long double a, long double b, long double c, int n, long double starting_point,
+                       long double ending_point);
+
+    void calculate_rectangular_method();
+
+    void calculate_trapezoidal_rule();
+
+};
+
+
+IntegralCalculator::IntegralCalculator(long double a, long double b, long double c, int n, long double starting_point,
+                                       long double ending_point) {
+    this->a = a;
+    this->b = b;
+    this->c = c;
+    this->starting_point = starting_point;
+    this->ending_point = ending_point;
+    this->n = n;
+    this->dx = (ending_point - starting_point) / n;
+}
+
+long double IntegralCalculator::calculate_formula(long double x) const {
     return a * powl(x, 2) + b * x + c;
 }
 
-void rectangular_method(int n, long double a, long double b, long double c, long double starting_point,
-                        long double ending_point) {
+void IntegralCalculator::calculate_rectangular_method() {
     printf("Calculating using rectangular method\n");
     long double calculated_integral = 0.0;
-    long double dx = (ending_point - starting_point) / n;
     long double start_time = omp_get_wtime();
 #pragma omp parallel for default(none) shared(a, b, c, starting_point, dx, n) reduction(+:calculated_integral)
     for (int i = 1; i < n; i++) {
-        calculated_integral += calculate_quadratic_formula(a, b, c, starting_point + i * dx) * dx;
+        calculated_integral += calculate_formula(starting_point + i * dx) * dx;
     }
     long double end_time = omp_get_wtime();
     printf("Calculation took %Lf seconds\n", end_time - start_time);
@@ -25,20 +53,16 @@ void rectangular_method(int n, long double a, long double b, long double c, long
            calculated_integral);
 }
 
-void trapezoidal_rule(int n, long double a, long double b, long double c, long double starting_point,
-                      long double ending_point) {
+void IntegralCalculator::calculate_trapezoidal_rule() {
     printf("Calculating using trapezoidal rule\n");
     long double calculated_integral = 0.0;
-    long double dx = (ending_point - starting_point) / n;
-    long double partial_value;
     long double start_time = omp_get_wtime();
-    long double value_for_start = calculate_quadratic_formula(a, b, c, starting_point);
-    long double value_for_end = calculate_quadratic_formula(a, b, c, ending_point);
+    long double value_for_start = calculate_formula(starting_point);
+    long double value_for_end = calculate_formula(ending_point);
     long double average = (value_for_start + value_for_end) / 2;
-    long double point;
-#pragma omp parallel for default(none) shared(a, b, c, starting_point, dx, n) private(partial_value, average, point) reduction(+:calculated_integral)
+#pragma omp parallel for default(none) shared(a, b, c, starting_point, dx, n) private(average) reduction(+:calculated_integral)
     for (int i = 1; i < n; i++) {
-        calculated_integral += (calculate_quadratic_formula(a, b, c, starting_point + (i * dx)));
+        calculated_integral += (calculate_formula(starting_point + (i * dx)));
     }
     calculated_integral += average;
     calculated_integral *= dx;
@@ -59,11 +83,12 @@ int main(int argc, char *argv[]) {
     long double end = result["e"].as<long double>();
     int processes = result["p"].as<int>();
     std::string method = result["m"].as<std::string>();
+    IntegralCalculator calculator(a, b, c, n, start, end);
     omp_set_num_threads(processes);
     if (method == "both" || method == "rec") {
-        rectangular_method(n, a, b, c, start, end);
+        calculator.calculate_rectangular_method();
     }
     if (method == "both" || method == "trap") {
-        trapezoidal_rule(n, a, b, c, start, end);
+        calculator.calculate_trapezoidal_rule();
     }
 }
